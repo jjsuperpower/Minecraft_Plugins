5e153af06787f11489faaa59560555929171ec85
jjsuperpower <jjs29356@gmail.com>
2022-12-08 20:26:44 -0600

    fixed some stupid errors

--- rl-craft-docker/root/mc_init.d/auto_chmod.sh
+++ rl-craft-docker/root/mc_init.d/auto_chmod.sh
@@ -3,8 +3,8 @@
 # this is needed if using bind mount, to make files writeable
 # its hacky, but it works
 while true; do
-    sleep 120
     chmod -R guo+rw ${MC_INST_DIR}
+    sleep 120
     # chmod 777 $(find ${MC_INST_DIR} -maxdepth 10 -type d | sed -e "s/'/'\\\\''/g;s/\(.*\)/'\1'/")       # the sed horror show is from stack exchange, long story short, don't put spaces in file names
     # chmod 666 $(find ${MC_INST_DIR} -maxdepth 10 -type f | sed -e "s/'/'\\\\''/g;s/\(.*\)/'\1'/")       # https://stackoverflow.com/questions/1616577/surround-all-lines-in-a-text-file-with-quotes-something
 done
